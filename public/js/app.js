b2d1651da83c5acd94633f22c1acc7d5a1a12acb
lyuba-petrova <lyba.petrova@gmail.com>
2020-06-17 15:20:25 +0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -15,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
 
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                messageOne.textContent = data.error
